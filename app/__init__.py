a7ad608856e548ae29335e872025e5ce301965c3
Lehith <lehith@gmail.com>
2020-11-07 19:39:38 +1000

    Мега-Учебник Flask, Часть 6

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 from config import Config
 
 app = Flask(__name__)
@@ -11,6 +11,16 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
+
+
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 
