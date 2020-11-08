d48c0564243fb5d5bc185d2fb357564dbf0fe209
Lehith <lehith@gmail.com>
2020-11-08 22:08:03 +1000

    Мега-Учебник Flask, Часть XI: Чуть-чуть косметики (издание 2018)

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -14,6 +15,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 
 
