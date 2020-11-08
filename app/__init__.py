967862224616e94a0073e75464fdcef07b782982
Lehith <lehith@gmail.com>
2020-11-08 21:58:59 +1000

    Мега-Учебник Flask, Часть X: Поддержка электронной почты (издание 2018)

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,21 @@
 from flask import Flask
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 from config import Config
+from flask_mail import Mail
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 
-# Запись последнего времени посещения для пользователя
-@app.before_request
-def before_request():
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()
-        db.session.commit()
 
 
 from app import routes, models, errors, logger
