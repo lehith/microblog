b4ab1dd439d19741d1e5a9a8988eee5433a47372
Lehith <lehith@gmail.com>
2020-11-09 16:40:06 +1000

    Мега-Учебник Flask, Часть XII: Даты и время (издание 2018)

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,14 @@
 from flask import Flask
-from flask_login import LoginManager
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
@@ -17,6 +16,8 @@ login.login_view = 'login'
 # login.login_message = "Пожалуйста, войдите, чтобы открыть эту страницу."
 mail = Mail(app)
 bootstrap = Bootstrap(app)
-
+moment = Moment(app)
 
 from app import routes, models, errors, logger
+
+
